d47bcd610e0420670cd7e23b4c13b6163cdddf83
chinhcn3 <chinhcn3@gmail.com>
2024-03-19 22:26:15 +0700

    Update base config

--- next.config.js
+++ next.config.js
@@ -6,14 +6,6 @@ const env = {
   ENVIRONMENT: process.env.ENVIRONMENT || '',
   APP_BE: process.env.APP_BE,
   APP_FE: process.env.APP_FE,
-  APP_FRONTSIDE_AGENT: process.env.APP_FRONTSIDE_AGENT || '/agent',
-  APP_AZURE_AD_TENANT_ID: process.env.APP_AZURE_AD_TENANT_ID,
-  APP_AZURE_AD_CLIENT_ID: process.env.APP_AZURE_AD_CLIENT_ID,
-  APP_CLIENT_ID: process.env.APP_CLIENT_ID,
-  APP_MFID_BASE: process.env.APP_MFID_BASE,
-  APP_NAVIS_BASE: process.env.APP_NAVIS_BASE,
-  APP_INQUIRY_LINK: process.env.APP_INQUIRY_LINK,
-  APP_ACCOUNTING_PLUS_BASE: process.env.APP_ACCOUNTING_PLUS_BASE,
 }
 
 const webpack = (config, _options) => {
@@ -45,10 +37,6 @@ const settings = {
   reactStrictMode: false,
   swcMinify: true,
   styledComponents: true,
-  assetPrefix: ['production', 'staging', 'beta'].includes(env.ENVIRONMENT)
-    ? `${env.APP_FE}/${env.ENVIRONMENT}/aweb`
-    : '',
-  productionBrowserSourceMaps: env.ENVIRONMENT !== 'production',
   env,
   webpack,
   compiler: {
